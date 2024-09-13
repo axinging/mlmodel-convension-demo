c79db0d930be60f7d7c632d0abbcc4a26ce0ab47
Xu Xing <xing.xu@intel.com>
2024-09-13 22:40:05 +0800

    Nit

--- onnx/jsonc2testdata/jsonc2testdata.py
+++ onnx/jsonc2testdata/jsonc2testdata.py
@@ -18,11 +18,11 @@ def buildAndRunBinaryGraph():
 
     t1 =  np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]).reshape((1, 3, 4, 1)).astype(np.float32)
     # The functional nodes:
-    n1 = helper.make_node(op, inputs=['a'], outputs=['output'], name='n1')
+    n1 = helper.make_node(op, inputs=['data'], outputs=['output'], name='n1')
     n1.attribute.extend([helper.make_attribute("perm", [0,2,1,3])])
     # Create the graph
     g1 = helper.make_graph([n1], 'preprocessing',
-                           [helper.make_tensor_value_info('a', DATA_TYPE, [1, 3, 4, 1])],
+                           [helper.make_tensor_value_info('data', DATA_TYPE, [1, 3, 4, 1])],
                            [helper.make_tensor_value_info('output', DATA_TYPE, [1, 4, 3, 1])])
     # Create the model and check
     m1 = helper.make_model(g1, producer_name='onnxtranspose-demo')
@@ -32,7 +32,7 @@ def buildAndRunBinaryGraph():
     MODEL_NAME = op+'_'+ comment +'.onnx'
     onnx.save(m1, MODEL_NAME)
     ort_sess = ort.InferenceSession(MODEL_NAME)
-    outputs = ort_sess.run(None, {'a': t1})
+    outputs = ort_sess.run(None, {'data': t1})
     # [1, 5, 9, 2, 6, 10, 3, 7, 11, 4, 8, 12],
     print(outputs)
     return MODEL_NAME
@@ -41,7 +41,7 @@ def buildAndRunBinaryGraph():
 inputs = {}
 inputs['data'] = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]).reshape((1, 3, 4, 1)).astype(np.float32)
 
-model_path = './models/Transpose_float.onnx' #buildAndRunBinaryGraph()
+model_path = buildAndRunBinaryGraph()
 print(model_path)
 patht = "test_transpose_broadcasetfail"
 ort_test_dir_utils.create_test_dir(model_path, 'temp/'+ patht, '', name_input_map=inputs)
