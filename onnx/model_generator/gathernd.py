a106cbdcf31c5ee71b58c041da24f17564676a71
Xu Xing <xing.xu@intel.com>
2024-11-06 14:44:47 +0800

    NIt

--- onnx/model_generator/gathernd.py
+++ onnx/model_generator/gathernd.py
@@ -4,14 +4,23 @@ from onnx import AttributeProto, GraphProto, OperatorSetIdProto, TensorProto, he
 import onnxruntime as ort
 
 MODEL_NAME = "gathernd"
-data_ =  np.array([[[0,1],[2,3]],[[4,5],[6,7]]]).reshape((2, 2, 2)).astype(np.int32)
-indices_ =  np.array([[1],[0]]).reshape((2, 1)).astype(np.int64)
+data_ = np.array([[[0, 1], [2, 3]], [[4, 5], [6, 7]]]
+                 ).reshape((2, 2, 2)).astype(np.int32)
+indices_ = np.array([[1], [0]]).reshape((2, 1)).astype(np.int64)
+
 
 def make_model(batch_dims):
     nodes = []
-    data = helper.make_tensor_value_info("data", TensorProto.INT32, [2,2,2])
-    indices = helper.make_tensor_value_info("indices", TensorProto.INT64, [2,1])
-    output = helper.make_tensor_value_info("output", TensorProto.INT32, [2,2])
+    data = helper.make_tensor_value_info("data", TensorProto.INT32, [2, 2, 2])
+    indices = helper.make_tensor_value_info(
+        "indices", TensorProto.INT64, [2, 1])
+    output = 0
+    if batch_dims == 0:
+        output = helper.make_tensor_value_info(
+            "output", TensorProto.INT32, [2, 2, 2])
+    if batch_dims == 1:
+        output = helper.make_tensor_value_info(
+            "output", TensorProto.INT32, [2, 2])
 
     gathernd_node = helper.make_node(
         "GatherND",
@@ -32,17 +41,21 @@ def make_model(batch_dims):
     opsets = []
     onnxdomain = OperatorSetIdProto()
     onnxdomain.version = 13
-    onnxdomain.domain = ""  # The empty string ("") or absence of this field implies the operator set that is defined as part of the ONNX specification.
+    # The empty string ("") or absence of this field implies the operator set that is defined as part of the ONNX specification.
+    onnxdomain.domain = ""
     opsets.append(onnxdomain)
 
     kwargs = {}
     kwargs["opset_imports"] = opsets
 
-    model_def = helper.make_model(graph_def, producer_name="onnx-example", **kwargs)
+    model_def = helper.make_model(
+        graph_def, producer_name="onnx-example", **kwargs)
     onnx.save(model_def, MODEL_NAME + str(batch_dims) + ".onnx")
+
 
 print("From 0:")
 print(data_[0:])
+print(data_[0:].shape)
 batch_dims = 0
 make_model(batch_dims)
 ort_sess = ort.InferenceSession(MODEL_NAME + str(batch_dims) + ".onnx")
@@ -52,6 +65,7 @@ print(outputs)
 
 print("From 1:")
 print(data_[1:])
+print(data_[1:].shape)
 
 batch_dims = 1
 make_model(batch_dims)
